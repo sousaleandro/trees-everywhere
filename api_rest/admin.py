0df542b6e1a8023789510a66320716523671662f
Leandro Sousa <lgleandro14@gmail.com>
2024-08-15 17:23:59 -0300

    feat: implements functions to accounts admin page such as activation and deactivation

--- api_rest/admin.py
+++ api_rest/admin.py
@@ -2,6 +2,24 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Account
 
+# Activate accounts action
+def activate_accounts(modeladmin, request, queryset):
+    queryset.update(active=True)
+
+activate_accounts.description = 'Activate account'
+
+# Deactivate accounts action
+def deactivate_accounts(modeladmin, request, queryset):
+    queryset.update(active=False)
+
+deactivate_accounts.description = 'Deactivate account'
+
+# Costumization of admin page for accounts
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created', 'active')
+    list_editable = ('active',)
+    actions = [activate_accounts, deactivate_accounts]
+
 # Add accounts to user in admin page
 class AccountInline(admin.TabularInline):
     model = Account.users.through
@@ -11,6 +29,6 @@ class CustomUserAdmin(UserAdmin):
     inlines = (AccountInline,)
 
 admin.site.register(User, CustomUserAdmin)
-admin.site.register(Account)
+admin.site.register(Account, AccountAdmin)
 
 admin.site.site_header = 'Trees Everywhere Admin'
