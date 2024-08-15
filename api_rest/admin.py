d6aa0aa0a599b7cc401a5864edeae3f03b79c891
Leandro Sousa <lgleandro14@gmail.com>
2024-08-15 16:11:52 -0300

    feat: create account model

--- api_rest/admin.py
+++ api_rest/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Account
 
 # + admin.site.site_header = 'Trees Everywhere Admin'
 admin.site.register(User, UserAdmin)
+admin.site.register(Account)
--- api_rest/models.py
+++ api_rest/models.py
@@ -7,3 +7,11 @@ class User(AbstractUser):
 
     def __str__(self):
         return f'Username: {self.username} - Email: {self.email}'
+
+class Account(models.Model):
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'Account: {self.name} - Active: {self.active}'
